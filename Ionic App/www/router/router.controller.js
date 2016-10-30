e2a15a7a4afadf3c21576367ffe5475f5b7f0d05
Christian Rodríguez <augusto.rodriguez@certicamara.com>
2016-10-30 12:51:59 -0500

    router movent

---
+++ Ionic App/www/router/router.controller.js
@@ -0,0 +1,47 @@
+baymaxPiApp.config(function ($stateProvider, $urlRouterProvider, USER_ROLES) {
+  $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+    })
+    .state('main', {
+      url: '/',
+      abstract: true,
+      templateUrl: 'templates/main.html'
+    })
+    .state('main.dash', {
+      url: 'main/dash',
+      views: {
+        'dash-tab': {
+          templateUrl: 'templates/dashboard.html',
+          controller: 'DashCtrl'
+        }
+      }
+    })
+    .state('main.paciente', {
+      url: 'main/paciente',
+      views: {
+        'paciente-tab': {
+          templateUrl: 'templates/paciente.html'
+        }
+      }
+    })
+    .state('main.admin', {
+      url: 'main/admin',
+      views: {
+        'admin-tab': {
+          templateUrl: 'templates/admin.html'
+        }
+      },
+      data: {
+        authorizedRoles: [USER_ROLES.admin]
+      }
+    });
+
+  // Thanks to Ben Noblet!
+  $urlRouterProvider.otherwise(function ($injector, $location) {
+    var $state = $injector.get("$state");
+    $state.go("main.dash");
+  });
+})
