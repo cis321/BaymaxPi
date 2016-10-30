ee550a32c0f27ed661951bec7ac12d1b52909d28
Christian Rodríguez <augusto.rodriguez@certicamara.com>
2016-10-30 17:26:24 -0500

    correct router

--- Ionic App/www/templates/componets/register/register.controller.js
+++ Ionic App/www/templates/componets/register/register.controller.js
@@ -2,17 +2,18 @@
 baymaxPiApp.controller('RegCtrl', function ($scope, $state, $ionicPopup, AuthService) {
     $scope.data = {};
 
-    $scope.register = function (data) {
-        AuthService.login(data.username, data.password).then(function (authenticated) {
-            $state.go('main.dash', {}, {
-                reload: true,
-            });
-            $scope.setCurrentUsername(data.username);
-        }, function () {
-            $ionicPopup.alert({
-                title: 'Login failed!',
-                template: 'Please check your credentials!',
-            });
-        });
-    };
+console.log('reg');
+    // $scope.register = function (data) {
+    //     AuthService.login(data.username, data.password).then(function (authenticated) {
+    //         $state.go('main.dash', {}, {
+    //             reload: true,
+    //         });
+    //         $scope.setCurrentUsername(data.username);
+    //     }, function () {
+    //         $ionicPopup.alert({
+    //             title: 'Login failed!',
+    //             template: 'Please check your credentials!',
+    //         });
+    //     });
+    // };
 })
